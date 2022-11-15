7f6735df7b53f0c4ba69e6d2bb85b6f9aa3ab630
gabriel-kimutai <gabrielkimutai997@gmail.com>
2022-11-16 00:43:08 +0300

    added dark mode

--- src/util/toggleDarkMode.ts
+++ src/util/toggleDarkMode.ts
@@ -1,11 +1,11 @@
 export const toggleDarkMode = () => {
   const root = document.getElementById('root');
   const indicator: HTMLElement | null = document.getElementById('indicator');
-  if (root!.classList.contains('dark')) {
+  if (!root!.classList.contains('dark')) {
     indicator!.style.transform = "translate(24px)";
     root!.classList.add('dark');
   } else {
     indicator!.style.transform = "translate(0px)";
     root!.classList.remove('dark');
-  }
+  };
 };1
